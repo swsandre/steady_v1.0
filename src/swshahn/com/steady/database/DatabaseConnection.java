d37fb56df6f6842e2fec2f612f411205096b90dc
swsandre <andre.hahn@sws-hahn.de>
2018-12-07 22:57:52 +0100

    DatabaseConnection.java and package created. swshahn.com.steady.gui package created.

---
+++ src/swshahn/com/steady/database/DatabaseConnection.java
@@ -0,0 +1,12 @@
+package swshahn.com.steady.database;
+
+/**
+ * Author: Andre Hahn
+ * Description: Creates the Database Connection (PostgreSQL)
+ *
+ * Change Log:
+ * 07.12.2018, Andre Hahn - Initial Creation
+ */
+
+public class DatabaseConnection {
+}
--- src/swshahn/com/steady/main/SteadyMain.java
+++ src/swshahn/com/steady/main/SteadyMain.java
@@ -1,4 +1,15 @@
 package swshahn.com.steady.main;
 
+/**
+ * Author: Andre Hahn
+ * Description: Entry point of the application.
+ *
+ * Change Log:
+ * 07.12.2018, Andre Hahn - Initial Creation
+ */
+
 public class SteadyMain {
+    public static void main(String args[]){
+        System.out.println("Steady - Accounting Application");
+    }
 }
