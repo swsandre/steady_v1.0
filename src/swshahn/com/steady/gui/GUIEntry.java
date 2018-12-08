d6bec75f84d43c616abe5c73c49989be0daef347
swsrene <rene.hahn@sws-hahn.de>
2018-12-08 12:23:53 +0100

    Class Comment for GUIEntry added

--- src/swshahn/com/steady/gui/GUIEntry.java
+++ src/swshahn/com/steady/gui/GUIEntry.java
@@ -1,4 +1,12 @@
 package swshahn.com.steady.gui;
 
+/**
+ * Author: Rene Hahn
+ * Description: GUI Entry Point for Steady Application
+ *
+ * Change Log:
+ * 08.12.2018, Rene Hahn- Initial Creation
+ */
+
 public class GUIEntry {
 }
